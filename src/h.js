6a1ed92396d7bc2c9610c9501d9d06423f885310
yelouafi <yelouafi@gmail.com>
2017-07-20 12:49:19 +0100

    h: fix nested text normalization

--- src/h.js
+++ src/h.js
@@ -1,4 +1,4 @@
-import { EMPTYO, EMPTYAR, isArray } from "./utils";
+import { EMPTYO, EMPTYAR, isArray, isVNode } from "./utils";
 
 export function h(type, props, contArg) {
   var content;
@@ -7,7 +7,7 @@ export function h(type, props, contArg) {
   if (len === 1) {
     if (isArray(contArg)) {
       content = maybeFlatten(contArg);
-    } else if (contArg && contArg._vnode) {
+    } else if (isVNode(contArg)) {
       content = [contArg];
     } else {
       content = [{ _text: contArg == null ? "" : contArg }];
@@ -34,8 +34,9 @@ export function h(type, props, contArg) {
 export function maybeFlatten(arr) {
   for (var i = 0; i < arr.length; i++) {
     var ch = arr[i];
-    if (isArray(ch)) return flattenChildren(arr, i, arr.slice(0, i));
-    else if (!ch || !ch._vnode) {
+    if (isArray(ch)) {
+      return flattenChildren(arr, i, arr.slice(0, i));
+    } else if (!isVNode(ch)) {
       arr[i] = { _text: ch == null ? "" : ch };
     }
   }
@@ -47,7 +48,7 @@ function flattenChildren(children, start, arr) {
     var ch = children[i];
     if (isArray(ch)) {
       flattenChildren(ch, 0, arr);
-    } else if (ch && ch._vnode) {
+    } else if (isVNode(ch)) {
       arr.push(ch);
     } else {
       arr.push({ _text: ch == null ? "" : ch });
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 export const EMPTYO = {};
 export const EMPTYAR = [];
 export const isArray = Array.isArray;
+export const isVNode = c => c && (c._vnode != null || c._text != null);
 export const isComponent = c => c && c.mount && c.patch && c.unmount;
 
 export const LOG = (...args) => {
